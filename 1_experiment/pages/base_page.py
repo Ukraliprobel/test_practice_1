1f8a50359af12895ac866bad376e51bf77bad705
Ukraliprobel <rws404@gmail.com>
2023-04-04 21:47:01 +0200

    added test_guest_should_see_login_link

--- 1_experiment/pages/base_page.py
+++ 1_experiment/pages/base_page.py
@@ -1,7 +1,17 @@
+from selenium.common.exceptions import NoSuchElementException #имя исключения
+
 class BasePage():
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)   #в конструктор BasePage добавим команду для неявного ожидания со значением по умолчанию в 10
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what):   #в нем будем перехватывать исключение с аргментами как искать(css, id, xpath и тд) и что искать(строку-селектор)
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException: #имя исключения
+            return False
+        return True
